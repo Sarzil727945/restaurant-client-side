aca0475d2c3af867410a3e8b0f3572ce2a80c166
Sarzil727945 <sarzilmuntaha.gmail.com>
2023-05-27 00:27:16 +0600

    Calus part added

--- src/components/Home/CallUs/CallUs.jsx
+++ src/components/Home/CallUs/CallUs.jsx
@@ -1,18 +1,18 @@
-// import React from 'react';
+import React from 'react';
 
-// const CallUs = () => {
-//      return (
-//           <div>
-//                <div className='text-center'>
-//                     <h4 className='pb-2 md:w-2/12 uppercase font-bold border-b-2 border-black mx-auto'>View Full  Menu</h4>
-//                </div>
-//                <div className=' bg-slate-900 text-white my-20'>
-//                     <h1 className=' font-bold text-4xl text-center py-36'>
-//                          Call Us: +88 01893254443
-//                     </h1>
-//                </div>
-//           </div>
-//      );
-// };
+const CallUs = () => {
+     return (
+          <div>
+               <div className='text-center'>
+                    <h4 className='pb-2 md:w-2/12 uppercase font-bold border-b-2 border-black mx-auto'>View Full  Menu</h4>
+               </div>
+               <div className=' bg-slate-900 text-white my-20'>
+                    <h1 className=' font-bold text-4xl text-center py-36'>
+                         Call Us: +88 01893254443
+                    </h1>
+               </div>
+          </div>
+     );
+};
 
-// export default CallUs;
+export default CallUs;
