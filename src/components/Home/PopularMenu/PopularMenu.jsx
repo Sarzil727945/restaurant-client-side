3cbbb6ffb31757d3168984c746948a33cca0e7a8
Sarzil727945 <sarzilmuntaha.gmail.com>
2023-05-27 00:30:12 +0600

    popularmenu added

--- src/components/Home/PopularMenu/PopularMenu.jsx
+++ src/components/Home/PopularMenu/PopularMenu.jsx
@@ -1,37 +1,37 @@
-// import React, { useEffect, useState } from 'react';
-// import SectionTitle from '../../../shared/SectionTitle/SectionTitle';
-// import MenuItem from '../../../shared/MenuItem/MenuItem';
+import React, { useEffect, useState } from 'react';
+import SectionTitle from '../../../shared/SectionTitle/SectionTitle';
+import MenuItem from '../../../shared/MenuItem/MenuItem';
 
-// const PopularMenu = () => {
-//      const [menu, setMenu] = useState([]);
+const PopularMenu = () => {
+     const [menu, setMenu] = useState([]);
 
-//      useEffect(()=>{
-//           fetch('menu.json')
-//           .then(res => res.json())
-//           .then(data => {
-//                const popularItem = data.filter(item => item.category === "popular")
-//                setMenu(popularItem)
-//           })
-//      }, [])
+     useEffect(()=>{
+          fetch('menu.json')
+          .then(res => res.json())
+          .then(data => {
+               const popularItem = data.filter(item => item.category === "popular")
+               setMenu(popularItem)
+          })
+     }, [])
 
-//      return (
-//           <div className=' mb-12'>
-//                <div>
-//                     <SectionTitle
-//                     heading = "FROM OUR MENU"
-//                     subHeading = "---Check it out---"
-//                     ></SectionTitle>
-//                </div>
-//                <div className=' grid lg:grid-cols-2 gap-10'>
-//                     {
-//                      menu.map(item => <MenuItem
-//                      key={item._id}
-//                      item = {item}
-//                      ></MenuItem>)    
-//                     }
-//                </div>
-//           </div>
-//      );
-// };
+     return (
+          <div className=' mb-12'>
+               <div>
+                    <SectionTitle
+                    heading = "FROM OUR MENU"
+                    subHeading = "---Check it out---"
+                    ></SectionTitle>
+               </div>
+               <div className=' grid lg:grid-cols-2 gap-10'>
+                    {
+                     menu.map(item => <MenuItem
+                     key={item._id}
+                     item = {item}
+                     ></MenuItem>)    
+                    }
+               </div>
+          </div>
+     );
+};
 
-// export default PopularMenu;
+export default PopularMenu;
