6885abd387bc7dfceee27da05bec6ef269478b86
Sarzil727945 <sarzilmuntaha.gmail.com>
2023-05-27 00:39:50 +0600

    home page complete

--- src/shared/MenuItem/MenuItem.jsx
+++ src/shared/MenuItem/MenuItem.jsx
@@ -1,19 +1,19 @@
-// import React from 'react';
+import React from 'react';
 
-// const MenuItem = ({ item }) => {
-//      const { name, image, price, recipe } = item
-//      return (
-//                <div className=' flex space-x-3'>
-//                     <div>
-//                          <img style={{ borderRadius: '0 200px 200px 200px' }} className=' w-[104px]' src={image} alt="" />
-//                     </div>
-//                     <div>
-//                          <h3 className=' uppercase'>{name}---------</h3>
-//                          <p>{recipe}</p>
-//                     </div>
-//                     <p className=' text-yellow-500'>${price}</p>
-//                </div>
-//      );
-// };
+const MenuItem = ({ item }) => {
+     const { name, image, price, recipe } = item
+     return (
+               <div className=' flex space-x-3'>
+                    <div>
+                         <img style={{ borderRadius: '0 200px 200px 200px' }} className=' w-[104px]' src={image} alt="" />
+                    </div>
+                    <div>
+                         <h3 className=' uppercase'>{name}---------</h3>
+                         <p>{recipe}</p>
+                    </div>
+                    <p className=' text-yellow-500'>${price}</p>
+               </div>
+     );
+};
 
-// export default MenuItem;
+export default MenuItem;
